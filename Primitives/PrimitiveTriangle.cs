131ba2b28efda1a0d787a28e06ac7d80c8d6b66e
Lutz Karau <karau_l@ers-hm.de>
2021-03-04 11:30:11 +0100

    Texturen hinzugefügt.

--- Primitives/PrimitiveTriangle.cs
+++ Primitives/PrimitiveTriangle.cs
@@ -12,9 +12,16 @@ namespace OpenGLTutorial.Primitives
 
         private static float[] _vertices = new float[]
         {
-            -0.5f, -0.5f, 0,
-            +0.5f, -0.5f, 0,
-            +0.0f, +0.5f, 0
+            -0.5f, -0.5f, 0, // 1. Punkt des Dreiecks
+            +0.5f, -0.5f, 0, // 2. Punkt des Dreiecks
+            +0.0f, +0.5f, 0  // 3. Punkt des Dreiecks
+        };
+
+        private static float[] _uvs = new float[]
+        {
+           0, 0,    // 1. Punkt (Texturkoordinate)
+           1, 0,    // 2. Punkt (Texturkoordinate)
+           0.5f, 1  // 3. Punkt (Texturkoordinate)
         };
 
         public static void Init()
@@ -27,6 +34,13 @@ namespace OpenGLTutorial.Primitives
             GL.BufferData(BufferTarget.ArrayBuffer, 9 * 4, _vertices, BufferUsageHint.StaticDraw);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
             GL.EnableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            int vboUVs = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboUVs);
+            GL.BufferData(BufferTarget.ArrayBuffer, 6 * 4, _uvs, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexAttribArray(1);
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 
             GL.BindVertexArray(0);
