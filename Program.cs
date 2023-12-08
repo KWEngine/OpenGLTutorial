028b2fde6d5e61e8c1b2f17863b8b539bca58846
Lutz Karau <lutz.karau@gmail.com>
2023-12-08 07:47:22 +0100

    fix: Updated System.Drawings.Common to 8.0

--- Program.cs
+++ Program.cs
@@ -8,12 +8,9 @@ namespace OpenGLTutorial
         static void Main(string[] args)
         {
             GameWindowSettings gws = new GameWindowSettings();
-            gws.IsMultiThreaded = false;
-            gws.RenderFrequency = 0;
             gws.UpdateFrequency = 0;
             NativeWindowSettings nws = new NativeWindowSettings();
             nws.Flags = OpenTK.Windowing.Common.ContextFlags.Debug;
-            nws.IsFullscreen = false;
             nws.NumberOfSamples = 0; // FSAA
             nws.Title = "Mein OpenGL-Projekt";
             nws.WindowBorder = OpenTK.Windowing.Common.WindowBorder.Resizable;
